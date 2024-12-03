b1289e6f81d1861a366306e474f7c84959a7780b
mix <max@maxsu.dev>
2024-12-02 20:18:57 -0500

    287 rm print for debugging

--- 287.find-the-duplicate-number.py
+++ 287.find-the-duplicate-number.py
@@ -1,16 +1,15 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        slow = fast = nums[0]
+        slow = fast = 0
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
             if slow == fast:
                 break
             
-        curr = nums[0]
+        curr = 0
         while True:
             curr = nums[curr]
             slow = nums[slow]
             if slow == curr:
                 return slow
-        return slow
