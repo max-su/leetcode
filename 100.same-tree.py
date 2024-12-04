619a702b57041e8e7bbd17d65f27f55559f54d28
mix <max@maxsu.dev>
2024-12-04 18:37:31 -0500

    100

---
+++ 100.same-tree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def is_same(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+            if bool(p) != bool(q):
+                return False
+            if not p and not q:
+                return True
+            return p.val == q.val and is_same(p.left, q.left) and is_same(p.right, q.right)
+        return is_same(p, q)
